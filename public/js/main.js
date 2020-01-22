6db5c295aa482ff79c69f094313abb4775a5cf24
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2020-01-22 14:34:45 -0800

    intro & photo, my story, contact form hidden feature added

--- public/js/main.js
+++ public/js/main.js
@@ -70,5 +70,41 @@ jQuery(document).ready(function ($) {
 
   });
 
+  // thank you for submission section
+
+  $('#contact').on('submit', function (e) {
+    e.preventDefault();
+
+    //get the name field value
+    var name = $('#name').val();
+    //get the name field value
+    // var email = $('#email').val();
+    // //get the comments
+    var message = $('#message').val();
+
+    //pretend we don't need validation
+
+    //send to formspree
+    $.ajax({
+      url: 'https://formspree.io/xwklygkj',
+      method: 'POST',
+      data: {
+        name: name,
+        // _replyto: email,
+        // email: email,
+        message: message,
+        _subject: 'My Form Submission',
+      },
+      dataType: "json",
+      success: function () {
+        console.log('success');
+        $('#formBlock').hide();
+        $('#thankyouBlock').show();
+      }
+
+    });
+
+  });
+
 
 });
